277db4e58de573ba203d4fb4f256fdb36e40bc28
Blagoja Stojev <duckpop@yahoo.com>
2025-05-12 21:02:38 +0200

    added staff page about the professor

--- JS.js
+++ JS.js
@@ -1,17 +1,8 @@
-// var TimeOut = null;
-// var NavRight = document.querySelector('.NavBarRight')
 var NavLinks = document.querySelector('.NavBarLinks')
 var DropDown = document.querySelector('.DropDown')
 
-// NavRight.addEventListener('mouseover', ()    => {
-//     clearTimeout(TimeOut)
-//     NavLinks.classList.add('show');
-// });
-// NavRight.addEventListener('mouseout', () => {
-//     TimeOut = setTimeout(() => {
-//         NavLinks.classList.remove('show');
-//     },150);
-// });
-DropDown.addEventListener('click', () =>{
-    NavLinks.classList.toggle('show')
-})
+if (DropDown && NavLinks) {
+    DropDown.addEventListener('click', () => {
+        NavLinks.classList.toggle('show');
+    });
+}
