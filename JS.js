6b3969893d32a42d5b5b14d5078311ddab345c86
Blagoja Stojev <duckpop@yahoo.com>
2025-05-13 22:12:13 +0200

    added projects page with card based display for projects

--- JS.js
+++ JS.js
@@ -6,3 +6,17 @@ if (DropDown && NavLinks) {
         NavLinks.classList.toggle('show');
     });
 }
+
+//making divs act like links for the project page
+
+function link(element){
+    const location = element.getAttribute('data-location')
+    const locationEnd = 'projects/' +location + '.html'
+    if(locationEnd){
+        window.location.href = locationEnd
+    }else {
+        console.log("no location found")
+    }
+}
+
+//
