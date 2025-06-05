6b824c321b86ee8826b98c5f42a16a49002b76a4
Blagoja Stojev <duckpop@yahoo.com>
2025-06-06 01:03:07 +0200

    added dark mode to the site

--- JS.js
+++ JS.js
@@ -1,3 +1,29 @@
+// setup
+document.addEventListener("DOMContentLoaded", function () {
+    var cookieVal = getCookieValue("darkMode")
+
+    if(cookieVal === null) {
+        var prefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+        if (prefersDark) {
+            document.cookie = "darkMode=true; path=/; max-age=31536000"
+            cookieVal = "true"
+        } else {
+            document.cookie = "darkMode=false; path=/; max-age=31536000"
+            cookieVal = "false"
+        }
+    }
+
+    if(cookieVal === "true"){
+        changeThemeCokkie()
+    }
+});
+
+function getCookieValue(name) {
+    const match = document.cookie.match(new RegExp('(^| )' + name + '=([^;]+)'));
+    return match ? decodeURIComponent(match[2]) : null;
+}
+
+// functions
 var NavLinks = document.querySelector('.NavBarLinks')
 var DropDown = document.querySelector('.DropDown')
 
@@ -19,4 +45,47 @@ function link(element){
     }
 }
 
+function changeThemeCokkie(){
+    var elements = document.getElementsByClassName("themeChangeNormal")
+    for (var i = 0; i < elements.length; i++){
+        elements[i].classList.toggle("TCN")
+    }
+
+    var elementsEnd = document.getElementsByClassName("themeChangeEnd")
+    for (var i = 0; i < elementsEnd.length; i++){
+        elementsEnd[i].classList.toggle("TCE")
+    }
+
+    var projects = document.getElementsByClassName("container-wrapper")
+
+    for (var i = 0; i < projects.length; i++){
+        projects[i].classList.toggle("TCN")
+    }
+}
+
 //
+function changetheme(){
+    var elements = document.getElementsByClassName("themeChangeNormal")
+    for (var i = 0; i < elements.length; i++){
+        elements[i].classList.toggle("TCN")
+    }
+
+    var elementsEnd = document.getElementsByClassName("themeChangeEnd")
+    for (var i = 0; i < elementsEnd.length; i++){
+        elementsEnd[i].classList.toggle("TCE")
+    }
+
+    var projects = document.getElementsByClassName("container-wrapper")
+
+    for (var i = 0; i < projects.length; i++){
+        projects[i].classList.toggle("TCN")
+    }
+
+    var cookieVal = getCookieValue("darkMode")
+
+    if (cookieVal === "true"){
+        document.cookie = "darkMode=false; path=/; max-age=31536000"
+    }else{
+        document.cookie = "darkMode=true; path=/; max-age=31536000"
+    }
+}
